e16d325a0dbbe75b13997e6d1f82ab6a7371ebe7
ViktorKula <vicyru@yandex.ru>
2024-08-23 05:24:53 +0300

    blood and sweat

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Weather from './Components/Weather/Weather.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Weather/>
+      
+    </div>
+  );
+}
+
+export default App;
